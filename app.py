b2745eabe66b3dc9bb9b313fe3590245aef9c60c
aartisolunke8-glitch <aartisolunke8@gmail.com>
2026-07-29 23:13:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,24 @@
 import streamlit as st
+import pandas as pd
 import joblib
-import numpy as np
 
-reg_scaler = joblib.load("regression_scaler.pkl")
-class_model = joblib.load("classification_model.pkl")
-reg_model = joblib.load("regression_model.pkl")
+pipe = joblib.load('full_pipeline.pkl')
 
-st.title("Multi-Model Prediction App")
+st.title("Student Math Score Prediction")
 
-problem = st.selectbox(
-    "Choose Problem Type",
-    ["Classification", "Regression"]
-)
+st.sidebar.header("Student Details")
+gender = st.sidebar.selectbox('Gender', ['female', 'male'])
+race = st.sidebar.selectbox('Race/Ethnicity', ['group A', 'group B', 'group C', 'group D', 'group E'])
+parent = st.sidebar.selectbox('Parental Education', ['some high school', 'high school', 'associates degree', 'some college', 'bachelors degree', 'masters degree'])
+lunch = st.sidebar.selectbox('Lunch', ['free/reduced', 'standard'])
+test = st.sidebar.selectbox('Test Preparation', ['none', 'completed'])
 
-if problem == "Classification":
-    st.subheader("Iris Classification")
+st.header("Scores")
+reading = st.slider('Reading Score', 0, 100, 72)
+writing = st.slider('Writing Score', 0, 100, 74)
 
-    f1 = st.number_input("Sepal Length")
-    f2 = st.number_input("Sepal Width")
-    f3 = st.number_input("Petal Length")
-    f4 = st.number_input("Petal Width")
-
-    if st.button("Predict"):
-        data = np.array([[f1, f2, f3, f4]])
-        result = class_model.predict(data)
-        st.success(result[0])
-
-else:
-    st.subheader("Student Math Score Prediction")
-
-    f1 = st.number_input("Gender")
-    f2 = st.number_input("Race/Ethnicity")
-    f3 = st.number_input("Parental Education")
-    f4 = st.number_input("Lunch")
-    f5 = st.number_input("Test Preparation")
-    f6 = st.number_input("Reading Score")
-    f7 = st.number_input("Writing Score")
-
-    if st.button("Predict"):
-        data = np.array([[f1, f2, f3, f4, f5, f6, f7]])
-        data = reg_scaler.transform(data)
-        result = reg_model.predict(data)
-        st.success(round(result[0], 2))
+if st.button('Predict'):
+    input_df = pd.DataFrame([[gender,race,parent,lunch,test,reading,writing]],
+        columns=['gender','race/ethnicity','parental level of education','lunch','test preparation course','reading score','writing score'])
+    pred = pipe.predict(input_df)
+    st.success(f"Predicted Math Score: {pred[0]:.2f} / 100")
